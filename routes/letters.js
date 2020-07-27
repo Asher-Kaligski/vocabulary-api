81a3088725c3a0f1a7894ffdbcfc5dbf1c77dbf3
asher <fullstack.number1@gmail.com>
2020-07-27 22:17:06 +0300

    the letters route has been updated

--- routes/letters.js
+++ routes/letters.js
@@ -92,8 +92,8 @@ router.patch('/:id', [auth, admin], async (req, res) => {
 
     res.send(word);
   } else {
-    Letter.update(
-      { _id: letter._id, 'words._id': req.body.wordId },
+    await Letter.update(
+      { '_id': letter._id, 'words._id': req.body.wordId },
       {
         $set: {
           'words.$.name': req.body.name,
