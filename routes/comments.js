4b5ddbfd71e2608b301ed2cc5ae682baaac3478e
asher <fullstack.number1@gmail.com>
2020-07-30 21:27:08 +0300

    the comments route has been updated

--- routes/comments.js
+++ routes/comments.js
@@ -32,7 +32,7 @@ router.get('/:id', [auth, admin], async (req, res) => {
   res.send(comment);
 });
 
-router.post('/', [auth], async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
